6db041bd0a5341c4d59b74069b55c97d9f4f2f99
cuentadetrabajo414 <cuentadetrabajo61@hotmail.com>
2026-08-30 03:54:48 +0000

    Fix password generator math imports

--- app/src/main/java/com/aura/defense/security/SecurePasswordGenerator.kt
+++ app/src/main/java/com/aura/defense/security/SecurePasswordGenerator.kt
@@ -1,5 +1,7 @@
 package com.aura.defense.security
 
+import kotlin.math.log2
+import kotlin.math.pow
 import kotlin.random.Random
 
 data class GeneratedPassword(
@@ -66,7 +68,7 @@ class SecurePasswordGenerator {
                                                                     val poolSize = (if (hasUpper) 26 else 0) + (if (hasLower) 26 else 0) + (if (hasDigit) 10 else 0) + (if (hasSymbol) 32 else 0)
                                                                             val entropy = if (poolSize > 0) length * kotlin.math.log2(poolSize.toDouble()) else 0.0
 
-                                                                                    val crackSeconds = kotlin.math.pow(2.0, entropy) / 1e10
+                                                                                    val crackSeconds = pow(2.0, entropy) / 1e10
                                                                                             val crackTime = when {
                                                                                                             crackSeconds < 1 -> "instantaneamente"
                                                                                                                         crackSeconds < 60 -> "${crackSeconds.toLong()} segundos"
