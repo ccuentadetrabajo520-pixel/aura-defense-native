8fc5bbaeb1044cf3ddb8b76c43a16e8b10d656aa
cuentadetrabajo123456 <rafaelmujica2000@gmail.com>
2026-09-17 00:07:28 +0000

    fix: ultima referencia size en AuraBot

--- app/src/main/java/com/aura/defense/ui/components/aura/AuraBot.kt
+++ app/src/main/java/com/aura/defense/ui/components/aura/AuraBot.kt
@@ -9,6 +9,7 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
